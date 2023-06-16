da329e5c202b99ffadf86da430bdd2c73c92cf86
Diiegohdez <Diiego.mtel@gmail.com>
2023-06-16 16:57:13 -0500

    correcion en slider home

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -1,20 +1,23 @@
 import React, { useState } from 'react';
 import './home.css';
 import Slider from "react-slick";
-import Slider2 from './slider';
 import { useInform } from '../hooks/useInform';
 import { Modal } from '../components/Modal';
+import formatterPeso from '../components/Formatter';
 
 
 const Home = () => {
 
+  //filtro de informacion del banner
   const { Datos } = useInform();
   const newDatos2 = Datos.filter(informacion => informacion.banner)
-
+  let newDatos = Datos.filter(produc => produc.price <= 50000);
+  
+  //Control del modal
   const [openModal, setOpenModal] = useState(false);
   const [itemProduc, setItemProduc] = useState();
 
-
+  //Configuracion del Slider
   const settings = {
 
     infinite: true,
@@ -24,6 +27,18 @@ const Home = () => {
     autoplay: true
   };
 
+  const settings2 = {
+
+    infinite: true,
+    slidesToShow: 3,
+    slidesToScroll: 1,
+    autoplay: true,
+    speed: 3000,
+    autoplaySpeed: 2000,
+    cssEase: "linear",
+
+  };
+
   return (
     <div className='home'>
       {openModal && <Modal item={itemProduc} setOpenModal={setOpenModal} />}
@@ -31,14 +46,25 @@ const Home = () => {
         <Slider  {...settings} className='Slider'>
           {newDatos2.map(item => (
             <div key={item._id} className="slider-item">
-              <img src={item.banner} alt='img' className="banner-img" onClick={()=>{setOpenModal(true);setItemProduc(item)}}/>
+              <img src={item.banner} alt='img' className="banner-img" onClick={() => { setOpenModal(true); setItemProduc(item) }} />
             </div>
           ))}
         </Slider>
       </div>
-      <Slider2/>
+      <div className='slider2'>
+        <Slider {...settings2} className="Slider-container">
+          {newDatos.map(item => (
+            <div key={item._id} className="slider2-item">
+              <img src={item.img} alt='img' className="slider2-img" />
+              <div className='slider2-btn'>
+                <p onClick={() => { setOpenModal(true); setItemProduc(item) }}>{formatterPeso.format(item.price)}</p>
+              </div>
+            </div>
+          ))}
+        </Slider>
+      </div>
     </div>
   );
 };
 
-export default Home;
+      export default Home;
